4bf6ec28a2dd7a6509c82f61fb3092a16dc7e2d1
Gonzalo Huerta Canepa <gonzalo@huerta.cl>
2019-12-26 17:51:11 -0300

    Django: Users with templates

--- django_orm/single_model_orm/users_app/views.py
+++ django_orm/single_model_orm/users_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
-# Create your views here.
+from users_app.models import *
+
+def index(request):
+  """
+
+  """  
+  users = User.objects.all()
+  context = {
+      'users': users
+  }
+  return render(request, 'index.html', context)
+
+def process(request):
+  """
+
+  """
+  first_name = request.POST['first_name_input']
+  last_name = request.POST['last_name_input']
+  email = request.POST['email_input']
+  age = request.POST['age_input']
+  User.objects.create(first_name=first_name, last_name=last_name, email_address=email, age=age)
+  return HttpResponseRedirect('/')
